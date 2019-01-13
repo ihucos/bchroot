eca8fbda1724adf5bfd380fff509f373d2f1f5f2
Irae Hueck Costa <mail@irae.me>
2019-01-13 22:47:14 +0000

    zudo: only unshare user if not root

--- zudo.c
+++ zudo.c
@@ -4,7 +4,7 @@ int main(int argc, char* argv[]) {
 	if (argc < 2){
 		brt_fatal("usage: zudo CMD1 [CMD2 ...]");
 	}
-	brt_setup_user_ns();
+	if (getuid()) brt_setup_user_ns();
 	argv++;
 	execvp(argv[0], argv);
 	brt_fatal("exec %s", argv[0]);
