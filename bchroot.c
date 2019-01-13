de824678d09ee8fd5e04dca5066bb19bb3f4c28f
Irae Hueck Costa <mail@irae.me>
2019-01-13 22:46:21 +0000

    -

--- bchroot.c
+++ bchroot.c
@@ -1,20 +1,4 @@
 #define _GNU_SOURCE
-#include <errno.h>
-#include <fcntl.h>
-#include <libgen.h>
-#include <limits.h>
-#include <pwd.h>
-#include <grp.h>
-#include <sched.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/mount.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include "brtlib.c"
 
 
