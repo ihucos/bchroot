53b1af08f47b05cc451c3c36f1684de089d99bea
Irae Hueck Costa <mail@irae.me>
2019-07-14 16:35:03 +0200

    -

--- bin/bchroot.c
+++ bin/bchroot.c
@@ -6,26 +6,35 @@
 
 #include "brtlib.h"
 
+#define OPT "n:m:e:E:"
+
 int
 main (int argc, char **argv)
 {
   int c;
   int has_mount = 0;
   int has_env = 0;
+  int no_user_ns = 0;
   char **exec_argv;
   char *bindto;
   char *rootfs;
 
-  if (getuid()) brt_setup_user_ns();
 
-  while ((c = getopt (argc, argv, "m:e:")) != -1)
+  while ((c = getopt (argc, argv, OPT)) != -1)
     switch (c) {
+      case 'n':
+        no_user_ns = 1;
       case 'm':
         has_mount = 1;
       case 'e':
         has_env = 1;
+      case 'E':
+        has_env = 1;
+      case '?':
+        break;
+      default:
+        abort ();
     }
-  if (opterr) return 1;
   if (! (rootfs = argv[optind])){
       fprintf(stderr, "bchroot: missing arg: rootfs\n");
       return 1;
@@ -35,17 +44,22 @@ main (int argc, char **argv)
       fprintf(stderr, "bchroot: missing arg: cmd\n");
       return 1;
   }
-  optind = 0;
+  optind = 1;
 
   if (has_mount) {
     brt_setup_mount_ns();
   }
+  if (!no_user_ns){
+      if (getuid()) brt_setup_user_ns();
+  }
 
-  while ((c = getopt (argc, argv, "m:")) != -1)
+  while ((c = getopt (argc, argv, OPT)) != -1)
     switch (c)
       {
       case 'e':
           brt_whitelist_env(optarg);
+      case 'E':
+          brt_whitelist_envs_from_env(optarg);
       case 'm':
         if (-1 == asprintf(&bindto, "%s/%s", rootfs, optarg)){
             perror("asprintf");
